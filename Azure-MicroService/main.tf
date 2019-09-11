18b656b4904d044723aabae159ca309e4a59a284
jamie.clayton <jamie.clayton@arq.group>
2019-09-11 21:59:05 +1000

    Move the resilience queue to the backend file
    * Microsoft queue is 'Service Bus'

--- Azure-MicroService/main.tf
+++ Azure-MicroService/main.tf
@@ -1,30 +1,4 @@
 
-# Resource Group to store all the related objects.
-resource "azurerm_resource_group" "envgrp" {  
-  name      = join("-", [var.Env, "rg"] )
-  location  = var.azLocation
-  tags      = var.azDefaultDevTags
-}
-
-# Queue services for Producer/Consumer software pattern.
-resource "azurerm_servicebus_namespace" "queue" {
-  name                = join("-", [ var.Env, "sbus" ] )
-  resource_group_name = azurerm_resource_group.envgrp.name
-  location            = azurerm_resource_group.envgrp.location
-  sku                 = "Standard"
-  tags                = var.azDefaultDevTags
-}
-
-resource "azurerm_servicebus_namespace_authorization_rule" "queuesecurity" {
-  name                = "RootManageSharedAccessKey"
-  namespace_name      = azurerm_servicebus_namespace.queue.name
-  resource_group_name = azurerm_resource_group.envgrp.name
-
-  listen = true
-  send   = true
-  manage = true
-}
-
 # Storeage account (LowerCaseOnly)
 resource "azurerm_storage_account" "storage" {
   name                     = lower(join( var.Env, "storage" ))
